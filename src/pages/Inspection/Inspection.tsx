8ba8e825fcc98ef65d4107a8fd431770e68dd073
Nabeel Hussain <nabeelbaig962@gmail.com>
2023-06-12 23:55:23 +0300

    Card will be tappable with action sheet as the event which will display job status options.

--- src/pages/Inspection/Inspection.tsx
+++ src/pages/Inspection/Inspection.tsx
@@ -1,44 +1,115 @@
 import {
+  IonActionSheet,
   IonButtons,
   IonCard, IonCardContent, IonCardHeader, IonCardSubtitle,
-  IonCardTitle, IonCol, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage,
-  IonRow, IonTitle, IonToolbar
+  IonCardTitle, IonCol, IonContent, IonHeader, IonList, IonMenu, IonMenuButton, IonPage,
+  IonRow, IonSelect, IonSelectOption, IonTitle, IonToolbar
 } from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
-import './Tab2.css';
+import '../Tab2.css';
+import { useState } from 'react';
 
 const Inspection: React.FC = () => {
+
+  const [selectListVisible, setSelectListVisible] = useState(false);
+
+  const inspectionMenu = () => {
+    setSelectListVisible(true);
+  };
+
+  const closeInspectionMenu = () => {
+    setSelectListVisible(false);
+  };
+
   return (
-    <><IonMenu contentId="main-content">
+    <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Inspection Menu</IonTitle>
+          <IonTitle>Inspection</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">This is the menu content.</IonContent>
-    </IonMenu><IonPage id="main-content">
-        <IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
           <IonToolbar>
-            <IonButtons slot="start">
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Inspection</IonTitle>
+            <IonTitle size="large">Inspection</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent className="ion-padding">
 
-          <h1>Entered</h1>
+        <h1>Entered</h1>
+        <IonCard color="light" button={true} onClick={inspectionMenu}>
+          <IonCardHeader>
+            <IonCardTitle>Card Title</IonCardTitle>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>Card Content</IonCardContent>
+        </IonCard>
 
-          <IonCard color="light">
-            <IonCardHeader>
-              <IonCardTitle>Card Title</IonCardTitle>
-              <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-            </IonCardHeader>
+        <IonActionSheet
+          isOpen={selectListVisible}
+          onDidDismiss={closeInspectionMenu}
+          cssClass="my-action-sheet"
+          buttons={[
+            {
+              text: 'In Progress',
+              handler: () => {
+                console.log('In Progress is selected');
+                close();
+              },
+            },
+            {
+              text: 'Inspection',
+              handler: () => {
+                console.log('Inspection is selected');
+                close();
+              },
+            },
+            {
+              text: 'On Hold',
+              handler: () => {
+                console.log('On Hold is selected');
+                close();
+              },
+            },
+            {
+              text: 'Quick Fix',
+              handler: () => {
+                console.log('Quick Fix is selected');
+                close();
+              },
+            },
+            {
+              text: 'Wash',
+              handler: () => {
+                console.log('Wash is selected');
+                close();
+              },
+            },
+            {
+              text: 'Done',
+              handler: () => {
+                console.log('Done is selected');
+                close();
+              },
+            },
+            {
+              text: 'Billed',
+              handler: () => {
+                console.log('Billed is selected');
+                close();
+              },
+            },
+            {
+              text: 'Cancelled',
+              handler: () => {
+                console.log('Cancelled is selected');
+                close();
+              },
+            }
+          ]}
+        />
 
-            <IonCardContent>Card Content</IonCardContent>
-          </IonCard>
-        </IonContent>
-      </IonPage></>
+      </IonContent>
+    </IonPage>
   );
 };
 
