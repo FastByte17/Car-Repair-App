a3f92e72cf65366f1f7aa44473131e93a7acb3e1
Nabeel_Hussain <nabeelbaig962@gmail.com>
2023-06-23 17:32:28 +0300

    created schema for tasks.

---
+++ backend/prisma/migrations/20230623142856_task_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "vehReg" VARCHAR(50) NOT NULL,
+    "note" TEXT NOT NULL,
+    "images" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
